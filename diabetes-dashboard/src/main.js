060793bc636a9c9d86b1c033f9e7e0f96cb7a079
gabrielazanova <gabrielavzanova@gmail.com>
2021-04-30 00:15:52 +0200

    Vue router implemented

--- diabetes-dashboard/src/main.js
+++ diabetes-dashboard/src/main.js
@@ -1,11 +1,21 @@
 import App from './App.vue';
 import Vue from 'vue' ;
 import vuetify from '@/plugins/vuetify';
+import { routes } from './router/router.js';
+import VueRouter from 'vue-router';
 
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 Vue.config.productionTip = false;
 
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: routes
+});
+
+
 new Vue({
     vuetify,
+    router,
     render: h => h(App)
   }).$mount('#app')
