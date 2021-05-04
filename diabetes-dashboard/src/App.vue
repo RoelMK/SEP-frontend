380e5edb56ef5cfc1348d3cfa20d6234064b41e1
Nikaels Balasovs <n.balasovs@student.tue.nl>
2021-05-04 16:11:36 +0200

    Bug fix and some refactoring

--- diabetes-dashboard/src/App.vue
+++ diabetes-dashboard/src/App.vue
@@ -1,86 +1,26 @@
 <template>
-  <div id="app">
-    <Header class="header"/>
-    <div class="clearfix"></div>
-    <div class="main">
-      <v-row>
-        <v-col class="col" cols="12" md="6">
-          <v-container>
-            <div class="col1">
-              <Glucose/>
-            </div>
-          </v-container>
-        </v-col>
-        <v-col class="col" cols="12" md="6">
-          <v-container>
-            <div class="col1">
-              <Profile/>
-            </div>
-          </v-container>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="col" cols="12" md="6">
-          <v-container>
-            <div class="col1">
-              <TableFoodData/>
-            </div>
-          </v-container>
-        </v-col>
-        <v-col class="col" cols="12" md="6">
-          <v-container>
-            <div class="col1 unalloc">
-              <TableActivitiesData/>
-            </div>
-          </v-container>
-        </v-col>
-      </v-row>
+    <div id="app">
+        <Header class="header"/>
+        <div class="clearfix"></div>
+        <router-view/>
+        <Footer/>
     </div>
-    <Footer/>
-  </div>
 </template>
 
 <script>
-import Footer from './components/Footer.vue'
-import Header from './components/Header.vue'
-import Glucose from './components/Glucose.vue'
-import Profile from './components/Profile.vue'
-import TableFoodData from './components/TableFoodData.vue'
-import TableActivitiesData from './components/TableActivitiesData.vue'
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
     Footer,
-    Header,
-    Glucose,
-    Profile,
-    TableFoodData,
-    TableActivitiesData
+    Header
   }
 }
 </script>
 
 <style>
-.unalloc {
-  min-height: 40vh;
-}
-.col {
-  padding: 5%;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-}
-.col1 {
-  border-radius: 20px;
-  left: 3%;
-  right: 3%;
-  background-color: white
-}
-.main {
-  background-color: #f2f2f2;
-  padding: 0 2% 0 2%;
-}
 .clearfix {
   height: 3vh;
   background-color: #f2f2f2;
---
+++ diabetes-dashboard/src/views/Dashboard.vue
@@ -0,0 +1,76 @@
+<template>
+    <div id="dashboard">
+        <div class="main">
+            <v-row>
+                <v-col class="col" cols="12" md="6">
+                    <v-container>
+                        <div class="col1">
+                            <Glucose/>
+                        </div>
+                    </v-container>
+                </v-col>
+                <v-col class="col" cols="12" md="6">
+                    <v-container>
+                        <div class="col1">
+                            <Profile/>
+                        </div>
+                    </v-container>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col class="col" cols="12" md="6">
+                    <v-container>
+                        <div class="col1">
+                            <TableFoodData/>
+                        </div>
+                    </v-container>
+                </v-col>
+                <v-col class="col" cols="12" md="6">
+                    <v-container>
+                        <div class="col1 unalloc">
+                            <TableActivitiesData/>
+                        </div>
+                    </v-container>
+                </v-col>
+            </v-row>
+        </div>
+    </div>
+</template>
+
+<script>
+import Glucose from '@/components/Glucose.vue';
+import Profile from '@/components/Profile.vue';
+import TableFoodData from '@/components/TableFoodData.vue';
+import TableActivitiesData from '@/components/TableActivitiesData.vue';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Glucose,
+    Profile,
+    TableFoodData,
+    TableActivitiesData
+  }
+}
+</script>
+
+<style>
+.unalloc {
+  min-height: 40vh;
+}
+.col {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.col1 {
+  border-radius: 20px;
+  left: 3%;
+  right: 3%;
+  background-color: white
+}
+.main {
+  background-color: #f2f2f2;
+  padding: 0 2% 0 2%;
+}
+</style>
