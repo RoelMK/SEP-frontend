b86311b03e7ce82ce8451dda723ab3d09f31ece2
gabrielazanova <gabvzanova@gmail.com>
2021-05-17 10:52:59 +0200

    apexchart heatmap implemented

--- diabetes-dashboard/src/components/HeatMapActivity.vue
+++ diabetes-dashboard/src/components/HeatMapActivity.vue
@@ -1,205 +1,99 @@
 <template>
   <div>
-    <VueChartHeatmap :entries="entries"></VueChartHeatmap>
+    <apexchart type="heatmap" height="350" :options="chartOptions" :series="series"></apexchart>
   </div>
 </template>
 
 <script>
-import VueChartHeatmap from "vue-chart-heatmap";
 export default {
   name: "activityMap",
-  components: {
-    VueChartHeatmap,
-  },
-  data() {
+  data: function () {
     return {
-      entries: [
+      chartOptions: {
+        dataLabels: {
+          enabled: false,
+        },
+        chart: {
+          toolbar: {
+            show: false,
+          },
+        },
+        colors: ["#47b784"],
+        xaxis: {
+          type: "category",
+        },
+      },
+      series: [
         {
-          counting: 2070,
-          created_at: "2021-01-21",
+          name: "Sun",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
         {
-          counting: 3493,
-          created_at: "2021-01-22",
+          name: "Sat",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
         {
-          counting: 1000,
-          created_at: "2021-01-24",
+          name: "Fri",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
         {
-          counting: 750,
-          created_at: "2021-01-23",
+          name: "Thu",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
         {
-          counting: 2070,
-          created_at: "2021-01-11",
+          name: "Wed",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
         {
-          counting: 2000,
-          created_at: "2021-01-10",
+          name: "Tue",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
         {
-          counting: 4500,
-          created_at: "2021-01-12",
+          name: "Mon",
+          data: this.generateData(24, {
+            min: 0,
+            max: 90,
+          }),
         },
-        {
-          counting: 2070,
-          created_at: "2020-06-21",
-        },
-        {
-          counting: 3493,
-          created_at: "2020-06-22",
-        },
-        {
-          counting: 1000,
-          created_at: "2020-06-24",
-        },
-        {
-          counting: 750,
-          created_at: "2020-06-23",
-        },
-        {
-          counting: 2070,
-          created_at: "2020-07-11",
-        },
-        {
-          counting: 2000,
-          created_at: "2020-07-10",
-        },
-        {
-          counting: 4500,
-          created_at: "2020-07-12",
-        },
-        {
-          counting: 349,
-          created_at: "2020-07-13",
-        },
-        {
-          counting: 988,
-          created_at: "2020-07-14",
-        },
-        {
-          counting: 3043,
-          created_at: "2020-07-15",
-        },
-        {
-          counting: 3400,
-          created_at: "2020-07-16",
-        },
-        {
-          counting: 5000,
-          created_at: "2020-08-01",
-        },
-        {
-          counting: 120,
-          created_at: "2020-08-02",
-        },
-        {
-          counting: 6304,
-          created_at: "2020-08-03",
-        },
-        {
-          counting: 876,
-          created_at: "2020-08-04",
-        },
-        {
-          counting: 5000,
-          created_at: "2020-08-05",
-        },
-        {
-          counting: 120,
-          created_at: "2020-08-06",
-        },
-        {
-          counting: 6304,
-          created_at: "2020-08-07",
-        },
-        {
-          counting: 500,
-          created_at: "2020-09-01",
-        },
-        {
-          counting: 1020,
-          created_at: "2020-09-02",
-        },
-        {
-          counting: 634,
-          created_at: "2020-09-03",
-        },
-        {
-          counting: 8706,
-          created_at: "2020-09-04",
-        },
-        {
-          counting: 5020,
-          created_at: "2020-09-05",
-        },
-        {
-          counting: 1207,
-          created_at: "2020-09-06",
-        },
-        {
-          counting: 6301,
-          created_at: "2020-09-07",
-        },
-        {
-          counting: 5010,
-          created_at: "2020-10-01",
-        },
-        {
-          counting: 120,
-          created_at: "2020-10-02",
-        },
-        {
-          counting: 6304,
-          created_at: "2020-10-03",
-        },
-        {
-          counting: 706,
-          created_at: "2020-10-04",
-        },
-        {
-          counting: 240,
-          created_at: "2020-10-05",
-        },
-        {
-          counting: 2107,
-          created_at: "2020-10-06",
-        },
-        {
-          counting: 538,
-          created_at: "2020-10-07",
-        },
-        {
-          counting: 500,
-          created_at: "2020-11-01",
-        },
-        {
-          counting: 1020,
-          created_at: "2020-11-02",
-        },
-        {
-          counting: 634,
-          created_at: "2020-11-03",
-        },
-        {
-          counting: 8706,
-          created_at: "2020-11-04",
-        },
-        {
-          counting: 5020,
-          created_at: "2020-11-05",
-        },
-        {
-          counting: 1207,
-          created_at: "2020-11-06",
-        },
-        {
-          counting: 6301,
-          created_at: "2020-11-07",
-        }
-      ]
+      ],
     };
+  },
+  methods: {
+    generateData(count, yrange) {
+      var i = 0;
+      var series = [];
+      while (i < count) {
+        var x = `${i}:00`.toString();
+        var y =
+          Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
+          yrange.min;
+
+        series.push({
+          x: x,
+          y: y,
+        });
+        i++;
+      }
+      return series;
+    },
   },
 };
 </script>
--- diabetes-dashboard/src/main.js
+++ diabetes-dashboard/src/main.js
@@ -4,11 +4,14 @@ import vuetify from '@/plugins/vuetify';
 import router from './router/router.js';
 import store from './store/store.js';
 import VCalendar from 'v-calendar';
+import VueApexCharts from "vue-apexcharts";
 
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 Vue.config.productionTip = false;
 
 Vue.use(VCalendar, { componentPrefix: 'vc' });
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 
 new Vue({
     store,
