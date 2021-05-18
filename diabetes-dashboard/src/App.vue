0e6b0b0cfc2aade71835fddddbb377d50ad1fbe8
jeroengi <jfmgijsbers@gmail.com>
2021-05-18 10:40:00 +0200

    Implemented very basic emotions component

--- diabetes-dashboard/src/App.vue
+++ diabetes-dashboard/src/App.vue
@@ -22,14 +22,13 @@ export default {
 
 <style>
 .clearfix {
-  height: 3vh;
+  height: 40px;
   background-color: #f2f2f2;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
