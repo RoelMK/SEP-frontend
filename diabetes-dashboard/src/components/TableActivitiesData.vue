cdf9988f02fea2c4158569ee5d1cfa3c0e3bbc4d
Vasil-VIS <vasilshteriyanov99@abv.bg>
2021-06-07 11:48:30 +0200

    added search bar to activities

--- diabetes-dashboard/src/components/TableActivitiesData.vue
+++ diabetes-dashboard/src/components/TableActivitiesData.vue
@@ -13,94 +13,169 @@
                 <v-container>
                     <v-row>
                         <v-col xs="10" sm="10" md="10" lg="10">
-                            <v-text-field v-model="search" label="Search"></v-text-field>
+                            <v-text-field
+                                v-model="search"
+                                label="Search"
+                            ></v-text-field>
                         </v-col>
                         <v-col><v-icon medium>mdi-plus</v-icon></v-col>
                     </v-row>
                 </v-container>
+            </template>
+
+            <template v-slot:[`body.prepend`]>
+                <tr>
+                    <td></td>
+                    <td>
+                        <v-text-field
+                            v-model="date"
+                            type="string"
+                            label="Before"
+                        ></v-text-field>
+                    </td>
+                    <td>
+                        <v-text-field
+                            v-model="startTime"
+                            type="string"
+                            label="Before"
+                        ></v-text-field>
+                    </td>
+                    <td>
+                        <v-text-field
+                            v-model="endTime"
+                            type="string"
+                            label="Before"
+                        ></v-text-field>
+                    </td>
+                    <td>
+                        <v-text-field
+                            v-model="calories"
+                            type="number"
+                            label="Less than"
+                        ></v-text-field>
+                    </td>
+                </tr>
             </template>
         </v-data-table>
     </div>
 </template>
 
 <script>
-import moment from 'moment';
+import Moment from "moment";
+import { extendMoment } from "moment-range";
+const moment = extendMoment(Moment);
 
 export default {
-    name: 'TableActivitiesData',
+    name: "TableActivitiesData",
     data() {
         return {
             headers: [
                 {
                     text: "Activities",
-                    value: "activity"
+                    value: "activity",
                 },
                 {
                     text: "Date",
-                    value: "date"
+                    value: "date",
+                    filter: (value) => {
+                        if (!this.date) return true;
+                        return (
+                            moment(value).format("L") <
+                            moment(this.date).format("L")
+                        );
+                    },
                 },
                 {
                     text: "Start Time",
-                    value: "startTime"
+                    value: "startTime",
+                    filter: (value) => {
+                        if (!this.startTime) return true;
+                        return (
+                            moment(value, "HH:mm").format("HH:mm") <
+                            moment(this.startTime, "HH:mm").format("HH:mm")
+                        );
+                    },
                 },
                 {
                     text: "End Time",
-                    value: "endTime"
+                    value: "endTime",
+                    filter: (value) => {
+                        if (!this.endTime) return true;
+                        return (
+                            moment(value, "HH:mm").format("HH:mm") <
+                            moment(this.endTime, "HH:mm").format("HH:mm")
+                        );
+                    },
                 },
                 {
                     text: "Calories",
-                    value: "calories"
-                }
+                    value: "calories",
+                    filter: (value) => {
+                        if (!this.calories) return true;
+                        return value < this.calories;
+                    },
+                },
             ],
+            search: "",
+            activityName: "",
+            date: "",
+            startTime: "",
+            endTime: "",
+            calories: "",
             activities: [
                 {
                     activity: "Run",
-                    date: moment().subtract(1,'day').format('L'),
-                    startTime: moment().subtract(24,'hours').format('HH:mm'),
-                    endTime: moment().subtract(23,'hours')
-                        .subtract(10,'minutes').format('HH:mm'),
-                    calories: 159
+                    date: moment().subtract(1, "day").format("L"),
+                    startTime: moment().subtract(24, "hours").format("HH:mm"),
+                    endTime: moment()
+                        .subtract(23, "hours")
+                        .subtract(10, "minutes")
+                        .format("HH:mm"),
+                    calories: 159,
                 },
                 {
                     activity: "Yoga",
-                    date: moment().subtract(1,'day').format('L'),
-                    startTime: moment().subtract(20,'hours').format('HH:mm'),
-                    endTime: moment().subtract(19,'hours')
-                        .subtract(30,'minutes').format('HH:mm'),
+                    date: moment().subtract(1, "day").format("L"),
+                    startTime: moment().subtract(20, "hours").format("HH:mm"),
+                    endTime: moment()
+                        .subtract(19, "hours")
+                        .subtract(30, "minutes")
+                        .format("HH:mm"),
                     calories: 100,
                 },
                 {
                     activity: "Chess",
-                    date: moment().subtract(1,'day').format('L'),
-                    startTime: moment().subtract(22,'hours').format('HH:mm'),
-                    endTime: moment().subtract(21,'hours').format('HH:mm'),
+                    date: moment().subtract(1, "day").format("L"),
+                    startTime: moment().subtract(22, "hours").format("HH:mm"),
+                    endTime: moment().subtract(21, "hours").format("HH:mm"),
                     calories: null,
                 },
                 {
                     activity: "Health check",
-                    date: moment().format('L'),
-                    startTime: moment().subtract(4,'hours').format('HH:mm'),
-                    endTime: moment().subtract(3,'hours')
-                        .subtract(30,'minutes').format('HH:mm'),
+                    date: moment().format("L"),
+                    startTime: moment().subtract(4, "hours").format("HH:mm"),
+                    endTime: moment()
+                        .subtract(3, "hours")
+                        .subtract(30, "minutes")
+                        .format("HH:mm"),
                     calories: null,
                 },
                 {
                     activity: "Ice hokey",
-                    date: moment().format('L'),
-                    startTime: moment().subtract(2,'hours').format('HH:mm'),
-                    endTime: moment().subtract(1,'hours').format('HH:mm'),
+                    date: moment().format("L"),
+                    startTime: moment().subtract(2, "hours").format("HH:mm"),
+                    endTime: moment().subtract(1, "hours").format("HH:mm"),
                     calories: 349,
                 },
             ],
-            search: ""
         };
     },
     computed: {
     },
     methods: {
         selectActivity(activity) {
-            this.$emit('selectedActivity', activity);
-        }
+            this.$emit("selectedActivity", activity);
+        },
     },
 };
 </script>
@@ -116,7 +191,7 @@ export default {
 
 .mdi-plus {
     border-radius: 50%;
-    padding: .2rem;
-    background: rgba(0, 0, 0, .15);
+    padding: 0.2rem;
+    background: rgba(0, 0, 0, 0.15);
 }
 </style>
