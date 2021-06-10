892fee939ad2cb43b2a2bc718bb26cdf6324c1e8
Vasil-VIS <vasilshteriyanov99@abv.bg>
2021-06-11 00:51:21 +0200

    groundwork for filtering

--- diabetes-dashboard/src/components/QueryMenu.vue
+++ diabetes-dashboard/src/components/QueryMenu.vue
@@ -2,10 +2,13 @@
     <v-dialog v-model="filter.show" max-width="800">
         <v-card>
             <v-card-title>Querying options</v-card-title>
+            {{this.data}}
             <v-card-text class="pb-0">
                 <v-row align="center" class="mx-0">
                     <v-col class="pl-0">
-                        <p v-if="selectedParameters.length <= 0" class="mt-5">No filters applied</p>
+                        <p v-if="selectedParameters.length <= 0" class="mt-5">
+                            No filters applied
+                        </p>
                         <div class="mb-3" v-else>
                             <v-chip
                                 v-for="(param, index) in selectedParameters"
@@ -34,7 +37,11 @@
                 <v-btn color="green darken-1" text @click="cancelFiltering">
                     Cancel
                 </v-btn>
-                <v-btn color="green darken-1" text @click="$store.dispatch('showFilter', { show: false })">
+                <v-btn
+                    color="green darken-1"
+                    text
+                    @click="filterData"
+                >
                     Apply Filters
                 </v-btn>
             </v-card-actions>
@@ -43,13 +50,17 @@
 </template>
 
 <script>
-import Query from '@/components/Query.vue';
-import properties from '@/components/configurations/queryProperties.js';
-import { mapState } from 'vuex';
+import Query from "@/components/Query.vue";
+import properties from "@/components/configurations/queryProperties.js";
+import { AxiosWrapper } from "@/helpers/wrapper.js";
+import { mapState } from "vuex";
+import moment from "moment";
+
+const wrapper = new AxiosWrapper();
 
 export default {
     components: {
-        Query
+        Query,
     },
     data() {
         return {
@@ -64,15 +75,16 @@ export default {
             },
             properties: properties,
             reload: false,
+            data: "",
         };
     },
     computed: {
-        ...mapState(['filter']),
+        ...mapState(["filter"]),
         selectedParameters() {
             return Object.entries(this.parameters)
-                .filter(f => f[1] !== null)
-                .map(d => d[0]);
-        }
+                .filter((f) => f[1] !== null)
+                .map((d) => d[0]);
+        },
     },
     methods: {
         updateParameters(index, value) {
@@ -83,19 +95,49 @@ export default {
                 this.parameters[element] = null;
             }
             this.reload = !this.reload;
-            this.$store.dispatch('showFilter', { show: false });
+            this.$store.dispatch("showFilter", { show: false });
         },
         capitalizeFirstLetter(str) {
             return str.charAt(0).toUpperCase() + str.slice(1);
-        }
-    }
+        },
+        filterData() {
+            let parameters = this.parameters;
+
+            let dataTypes = [];
+            let startDate = moment.utc(parameters.date.start)
+                .format("DD-MM-yyyy");
+            let endDate = moment.utc(parameters.date.end).format("DD-MM-yyyy");
+            //let startTime = moment.utc(parameters.time.start).format("HH:mm");
+            //let endTime = moment.utc(parameters.time.end).format("HH:mm");
+
+            if(parameters.glucose != null) {
+                dataTypes.push("glucose", "insulin");
+            }
+            if(parameters.arousal != null || parameters.valence != null) {
+                dataTypes.push("mood");
+            }
+            if(parameters.activity != null) {
+                dataTypes.push("exercise");
+            }
+            if(parameters.food != null) {
+                dataTypes.push("food");
+            }
+
+            wrapper.get(`http://localhost:8080/data?startDate=${startDate}
+                &endDate=${endDate}&dataTypes=${dataTypes}
+                &union=${false}`, (dataPromise) => dataPromise)
+                .then((data) => {this.data = data;});
+
+            this.cancelFiltering();
+        },
+    },
 };
 </script>
 
 <style>
 .mdi-plus {
-  border-radius: 50%;
-  padding: 0.2rem;
-  background: rgba(0, 0, 0, 0.15);
+    border-radius: 50%;
+    padding: 0.2rem;
+    background: rgba(0, 0, 0, 0.15);
 }
 </style>
